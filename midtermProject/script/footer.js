837156868dfb1c2ad9b62d299cea0fc20b00b6ad
poon.n <moopoon.n@gmail.com>
2023-10-23 00:10:09 +0700

    fix wording

--- midtermProject/script/footer.js
+++ midtermProject/script/footer.js
@@ -17,7 +17,7 @@
           <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
           </a>
-          <span class="mb-3 mb-md-0 text-body-secondary">© 2023 LOOBOOK</span>
+          <span class="mb-3 mb-md-0 text-body-secondary">© 2023 CAPSULE</span>
         </div>
         <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
           <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
